bf7b5f09006fcd72e57292ce206e02aa1e96fafd
Adam Cooke <adam@atechmedia.com>
2009-05-08 15:36:19 +0100

    initial commit

---
+++ bin/htmlserve.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'sinatra'
+
+set :port, ARGV[0] || 4567
+
+get '*' do
+  file = File.join('.', params[:splat].first)
+  if File.exist?(file) && !File.file?(file)
+    file = File.join(file, 'index.html')
+  end
+  
+  if File.file?(file)
+    properties = `file -b -i #{file}`.chomp
+    file_contents = File.read(file)
+    
+    headers(
+      'Content-Type' => properties, 
+      'Server' => "HTMLServe", 
+      'Date' => Time.now.httpdate,
+      'Content-Length' => file_contents.length,
+      'Connection' => 'close'
+      )
+
+    file_contents
+  else
+    not_found("<p><span style='color:red'><b>Page Not Found</b> at #{file}</span></p>")
+  end
+end
---
+++ htmlserve.gemspec
@@ -0,0 +1,16 @@
+Gem::Specification.new do |s|
+  s.name = 'htmlserve'
+  s.version = "1.0.0"
+  s.platform = Gem::Platform::RUBY
+  s.summary = "A simple sinatra based server for rendering HTML sites"
+  
+  s.files = ['bin/htmlserve', 'bin/htmlserve.rb']
+  s.has_rdoc = false
+
+  s.bindir = "bin"
+  s.executables << "htmlserve"
+
+  s.author = "Adam Cooke"
+  s.email = "adam@atechmedia.com"
+  s.homepage = "http://www.atechmedia.com"
+end
